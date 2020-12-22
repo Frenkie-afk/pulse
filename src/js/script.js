c327358fcd3f824c70032ec739b5b42b2534253a
Sergey <example@gmail.com>
2020-12-22 13:07:07 +0200

    step 8

--- src/js/script.js
+++ src/js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__slide').eq(i).toggleClass('catalog-item__slide_active');
             })
         })
     
@@ -110,6 +110,8 @@ $(document).ready(function(){
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
+
+    new WOW().init();
 
   });
 
